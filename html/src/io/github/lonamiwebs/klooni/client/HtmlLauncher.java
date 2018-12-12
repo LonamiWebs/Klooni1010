dee6077a63483696521e88e4d8c85cba329813d2
TacoTheDank <SkytkRSfan3895@gmail.com>
2018-12-12 17:43:18 -0500

    "html" module (5/6) - java 1.8 compatibility - reformat code - update gwt 2.8.0 -> 2.8.2 (hopefully that's ok) - use import for GwtSuperDev
    Signed-off-by: TacoTheDank <SkytkRSfan3895@gmail.com>

--- html/src/io/github/lonamiwebs/klooni/client/HtmlLauncher.java
+++ html/src/io/github/lonamiwebs/klooni/client/HtmlLauncher.java
@@ -20,17 +20,18 @@ package io.github.lonamiwebs.klooni.client;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+
 import io.github.lonamiwebs.klooni.Klooni;
 
 public class HtmlLauncher extends GwtApplication {
 
     @Override
-    public GwtApplicationConfiguration getConfig () {
+    public GwtApplicationConfiguration getConfig() {
         return new GwtApplicationConfiguration(Klooni.GAME_WIDTH, Klooni.GAME_HEIGHT);
     }
 
     @Override
-    public ApplicationListener createApplicationListener () {
+    public ApplicationListener createApplicationListener() {
         return new Klooni(null);
     }
 }
